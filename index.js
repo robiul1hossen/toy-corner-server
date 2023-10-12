15af24d20572c52e844e389449512305789c9ec4
Mohammad Robiul Hossen <robim6225@gmail.com>
2023-10-12 15:26:42 +0600

    get api for specific toy details using id

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dowmgti.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -64,6 +64,15 @@ async function run() {
           .status(500)
           .json({ error: "An error occurred while fetching data." });
       }
+    });
+
+    // get toy details by id
+    app.get("/toy-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const result = await alltoysCollection.findOne(filter);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
